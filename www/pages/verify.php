593edb290efa811fc42ca01a4da5e275305c9dd0
Niamh-Griffin <niamh.griffin@student.ncirl.ie>
2015-12-05 17:14:42 +0000

    tidied up whole page

--- www/pages/verify.php
+++ www/pages/verify.php
@@ -12,40 +12,28 @@ $res=mysql_query("SELECT * FROM users WHERE user_id=".$_SESSION['user']);
 <!DOCTYPE html>
 <html lang="en" ng-app="myApp">
   <head>
-    <meta charset="utf-8">
+    <title>Bobo App</title>
+	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	
-	<meta name="description" content="verify">
+    <meta name="description" content="verify">
     <meta name="author" content="Youcef">
-    <link rel="icon" href="../images/BoboLogo.png"><!--picture on tab beside title-->
-
-    <title>Bobo App</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.css" rel="stylesheet">
-
-    <!-- error handling -->
-	<script src="assets/js/ie-emulation-modes-warning.js"></script>
-	
-	<!-- Latest compiled and minified JavaScript -->
-	<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>-->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	<!-- AngularJS Link---->
-	<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
-
-	<!--Own CSS-->
-	<script src="../css/search.css"></script>
-	
+	<!--picture on tab beside title-->
+	<link rel="icon" href="../images/BoboLogo2.ico">
+	<!-- Bootstrap core CSS -->
+	<link href="../css/bootstrap.css" rel="stylesheet">
 	
 </head>
 
 <body>
+	
+	<!--This is the Backstretch code which uses a jquery-->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script type="text/javascript" src="../jquery/jquery.backstretch.js"></script>
+	<script type="text/javascript">
+		$.backstretch(["../images/cab.jpg"]);
+	</script>
 
 		<!--this is the code for the navbar-->
 		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -85,73 +73,52 @@ $res=mysql_query("SELECT * FROM users WHERE user_id=".$_SESSION['user']);
 			<div id="login-form">
 				<form method="post">
 						<div  ng-controller="customersCtrl" > 
-						
-								<label>SEARCH &nbsp;&nbsp; <input placeholder="Taxi Reg/Reference No" ng-model="searchText" ></label>
-										
-							    <br> <table class="table" ng-hide="!searchText.length" style="background-color:white; opacity:0.8; font-weight:bold; font-size:18px">
-								<tr>
-									<th>Profile Photo</th>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Registration</th>
-								</tr>
-								<tr ng-repeat="x in drivers | filter:searchText | orderBy:'last_name' "><br>
-								
-								<td><img ng-src="{{image}}"/>
-								<td>{{ x.first_name}}</td>
-								<td>{{ x.last_name}}</td>
-									<td>{{ x.taxi_reg}}</td>
-							   </tr>
-							   <tr colspan ="4">
-							   <td><p>&nbsp;&nbsp;Accept taxi</p>
-							   <td ><button class= "btn btn-danger" onClick="history.go(0)">&nbsp;&nbsp;No&nbsp;&nbsp;</button></td>
-							   <td> 
-							   <a type="submit" class= "btn btn-success" href="msgLeaving.php?user_id=<?php echo $userRow['user_id'];?>&taxi_reg={{searchText}}">Yes</a>
-							  					   
-							   </tr>
+							<label>SEARCH &nbsp;&nbsp; <input placeholder="Taxi Reg/Reference No" ng-model="searchText" ></label>
+							<br>
+								<table class="table" ng-hide="!searchText.length" style="background-color:white; opacity:0.8; font-weight:bold; font-size:18px">
+									<tr>
+										<th>Profile Photo</th>
+										<th>First Name</th>
+										<th>Last Name</th>
+										<th>Registration</th>
+									</tr>
+									<tr ng-repeat="x in drivers | filter:searchText | orderBy:'last_name' "><br>
+										<td><img ng-src="{{image}}"/>
+										<td>{{ x.first_name}}</td>
+										<td>{{ x.last_name}}</td>
+											<td>{{ x.taxi_reg}}</td>
+									</tr>
+									<tr colspan ="4">
+									   <td><p>&nbsp;&nbsp;Accept taxi</p>
+									   <td ><button class= "btn btn-danger" onClick="history.go(0)">&nbsp;&nbsp;No&nbsp;&nbsp;</button></td>
+									   <td> 
+										<a type="submit" class= "btn btn-success" href="msgLeaving.php?user_id=<?php echo $userRow['user_id'];?>&taxi_reg={{searchText}}">Yes</a>
+									</tr>
 								</table>
+						</div>
+					</form>
+				</div> <!--end of login form-->	
+			</div> <!--end of jumbo-->
+		</div><!--end of container-->
+						
 				
-						
-</div>
-					
-
-				</form>
-			</div>
-						
-			<footer class="navbar-default navbar-fixed-bottom">
-			<div class="container-fluid"> 
-			<center>
-				<span><p>Copyright &copy; <a href="bobo15@gmail.com" title="bobo15@gmail.com">bobo15@gmail.com</a></p></span>
-			</center>
-			</div>
-	</footer>
-			
-		</div>	
-			
-	</div>	
         
-         
-
-
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.7/angular.min.js"></script>
-	
-	
-	
+    <!--this is the code for the footer navbar-->
+	<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<center><h4>© 2015 Bobo App</h4></center>
+			</div>
+		</div>
+	</nav>
+        
+    	<!--angluar script file-->
+	<script src="../javaScript/angular.min.js"></script>
 	<!-- Bootstrap core JavaScript
 	================================================= -->
 	<!-- Placed at the end of the document so the pages load faster -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="../dist/js/bootstrap.min.js"></script>
-	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-	<script src="../assets/js/ie10-viewport-bug-workaround.js"></script>
-	
-	<!--This is the Backstretch code which uses a jquery-->
-			<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-			<script type="text/javascript" src="../jquery/jquery.backstretch.js"></script>
-			<script type="text/javascript">
-				$.backstretch(["../images/cab.jpg"]);
-			</script>
-	
+	<script src="../javaScript/bootstrap.min.js"></script>
 	<script src="../javaScript/bobo.js"></script>
 	
  
